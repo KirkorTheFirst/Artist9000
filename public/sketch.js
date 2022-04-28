fd666aa5b7faa0c6473eef191fe1eac7e737e2db
Finn Taylor <beans@cjtaylors-MacBook-Pro.local>
2022-04-28 09:09:09 -0700

    added moar

--- public/sketch.js
+++ public/sketch.js
@@ -1,13 +1,14 @@
+//p5js functions:
 function setup() {
-    let c = createCanvas(window.innerWidth, window.innerHeight);
-    stroke(0, 0, 0);
-    strokeWeight(15);
-    c.parent('canvas');
-  }
-  
-  function draw() {
-      if(mouseIsPressed) {
-          line(mouseX, mouseY, pmouseX, pmouseY);
-      }
-    return false;
-  }
+    let c = createCanvas(windowWidth, windowHeight)
+    background(220)
+    stroke(0, 0, 0)
+    strokeWeight(7)
+    c.parent('canvas')
+}
+function draw() {
+    if(mouseIsPressed) {
+        line(mouseX, mouseY, pmouseX, pmouseY)
+    }
+    return false
+}
