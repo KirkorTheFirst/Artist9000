93b1e609c414a7890624a1e2e5a137941c516ee4
Jonathand2024 <Jonathand2024@headroyce.org>
2022-04-29 08:44:18 -0700

    Pushed to dumb idiot git cuz finn and lucas suck

---
+++ public/javascript/events.js
@@ -0,0 +1,16 @@
+// document.getElementById("paint").oninput = function(){
+//     let r = parseInt(this.value.substring(1, 3), 16);
+//     let g = parseInt(this.value.substring(3, 5), 16);
+//     let b = parseInt(this.value.substring(5, 7), 16);
+//     changeColor(r, g, b);
+// }
+
+// document.getElementById("submit").onclick = function(){
+//     window.location.href='results.html';
+
+// }
+
+document.getElementById("back").onclick = function(){
+    window.location.href='sketch.html';
+    console.log("test");
+}
---
+++ public/javascript/logic.js
@@ -0,0 +1,7 @@
+// here we put the code for the algorithm that takes in an image, compares it to google's dataset, and returns a priority queue
+
+// Heap rtn = new Heap();
+
+//when the code is run givenPrompt should not be null... check if it is and say "oops we couldnt do it boohoo"
+let givenPrompt = null;
+
