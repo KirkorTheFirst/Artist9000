bff50d38328e2e2df5f3a13b56dd522af8ab795f
Finn Taylor <beans@cjtaylors-MacBook-Pro.local>
2022-05-02 10:59:04 -0700

    edited little bit of css, added all the items to the randomiser

--- public/javascript/logic.js
+++ public/javascript/logic.js
@@ -1,6 +1,4 @@
 // here we put the code for the algorithm that takes in an image, compares it to google's dataset, and returns a priority queue
-
-// Heap rtn = new Heap();
-
+// let rtn = new Heap();
 //when the code is run givenPrompt should not be null... check if it is and say "oops we couldnt do it boohoo"
 let givenPrompt = null;
